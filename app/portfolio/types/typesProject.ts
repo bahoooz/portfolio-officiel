e909d61bb05d47a607c49d00a9cb04f23b25ad91
Bahoz <julienbraultcherif@gmail.com>
2024-10-03 07:59:24 +0200

    fix error typedProjects

--- app/portfolio/types/typesProject.ts
+++ app/portfolio/types/typesProject.ts
@@ -3,18 +3,13 @@ export type Projects = {
 };
 
 export type Project = {
-  link: string;
   title: string;
+  link: string;
   img_primary: string;
   width_img: number;
   height_img: number;
   color_bg: string;
   description: string;
-  techno_1: string;
-  techno_2: string;
-  techno_3: string;
-  techno_4: string;
-  img_1: string;
-  img_2: string;
-  img_3: string;
+  technos: string[];
+  website_link: string;
 };
