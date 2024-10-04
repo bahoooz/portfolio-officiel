d4cd35895f36b4ebd3de2ebf0ad838ebc69c7fda
Bahoz <julienbraultcherif@gmail.com>
2024-10-04 04:14:21 +0200

    maj metadatas layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,6 +11,46 @@ export const metadata: Metadata = {
   icons: {
     icon: "/assets/diamond_icon.png",
   },
+  keywords: [
+    "Développeur web",
+    "Portfolio",
+    "Paris",
+    "Bahoz",
+    "Web development",
+    "Freelance",
+  ],
+  authors: [{ name: "Bahoz" }],
+  openGraph: {
+    title: "Accueil - Portfolio Bahoz",
+    description:
+      "Développeur web parisien de 18 ans, découvrez mon portfolio et travaillons ensemble !",
+    url: "https://www.bahoz-dev.com",
+    siteName: "Portfolio Bahoz",
+    images: [
+      {
+        url: "/assets/diamond_icon.png",
+        width: 1200,
+        height: 630,
+        alt: "Portfolio Bahoz",
+      },
+    ],
+    locale: "fr_FR",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Accueil - Portfolio Bahoz",
+    description:
+      "Découvrez le portfolio de Bahoz, un développeur web parisien de 18 ans, et commencez à travailler ensemble sur vos projets.",
+    images: ["/assets/diamond_icon.png"],
+  },
+  robots: {
+    index: true,
+    follow: true,
+  },
+  alternates: {
+    canonical: "https://www.bahoz-dev.com",
+  },
 };
 
 export default function RootLayout({
