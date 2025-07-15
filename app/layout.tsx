5c8d2ae1a71a93b40bae62f865a47eddfc968e56
Bahoz <julienbraultcherif@gmail.com>
2025-07-16 01:49:49 +0200

    add logos to metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,13 @@ import ClientLayout from "./components/ClientLayout";
 
 export const metadata: Metadata = {
   icons: {
-    icon: "/assets/favicon.ico",
+    icon: [
+      { url: "/assets/favicon.ico", sizes: "32x32", type: "image/x-icon" },
+      { url: "/assets/logo_192x192.png", sizes: "192x192", type: "image/png" },
+      { url: "/assets/logo_512x512.png", sizes: "512x512", type: "image/png" },
+    ],
+    shortcut: "/assets/favicon.ico",
+    apple: "/assets/apple-touch-icon.png"
   },
 };
 
